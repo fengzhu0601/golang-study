230c3a9c4966c8734da6efb4979877970261d1ec
fengzhu <freemanfeng@acingame.com>
2018-12-27 21:32:32 +0800

    update

--- redis/redisTest.go
+++ redis/redisTest.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/go-redis/redis"
+	"encoding/json"
+	"slg_game_server/server/goredis"
+	"slg_game_server/server/util"
 )
 
 func main()  {
 	client := createClient()
 	defer client.Close()
+	testStruct()
 }
 
 // 创建 redis 客户端
@@ -23,4 +27,25 @@ func createClient() *redis.Client {
 	fmt.Println( pong, err )
 
 	return client
+}
+
+type Land struct {
+	LandId		int32		// 地块ID
+	OwnerId 	int32		// 归属玩家
+	MarchMap	map[MarchKey]int32	// 行军列表：[玩家id+部队id]部队行军信息
+}
+
+type MarchKey struct {
+	UserId		int32		// 玩家ID
+	ArmyId		int32		// 部队ID
+}
+func testStruct() {
+	landObj := &Land{LandId:100, OwnerId:200, MarchMap:make(map[MarchKey]int32)}
+	marchKey := MarchKey{UserId:200, ArmyId:300}
+	landObj.MarchMap[marchKey] = 300
+	marchJson, _ := json.Marshal(landObj.MarchMap)
+
+	m := map[string]interface{}{"OwnerId":landObj.OwnerId, "LandId":landObj.LandId, "MarchMap": marchJson}
+	goredis.ClientRedis.HMSet("land" + util.ToStr(landObj.LandId), m)
+	fmt.Println("-------------put land : ", landObj)
 }
